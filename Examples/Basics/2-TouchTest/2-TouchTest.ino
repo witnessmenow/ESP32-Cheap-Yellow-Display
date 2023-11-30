e768ffae3982e4aee38f3a6c356ce0b41effee78
Claus Näveke <github@naeveke.de>
2023-11-30 17:54:03 +0100

    Fix SPI usage in 2-TouchTest.ino
    Since HSPI is used by the screen, touch has to use VSPI

--- Examples/Basics/2-TouchTest/2-TouchTest.ino
+++ Examples/Basics/2-TouchTest/2-TouchTest.ino
@@ -53,7 +53,7 @@
 
 // ----------------------------
 
-SPIClass mySpi = SPIClass(HSPI);
+SPIClass mySpi = SPIClass(VSPI);
 XPT2046_Touchscreen ts(XPT2046_CS, XPT2046_IRQ);
 
 TFT_eSPI tft = TFT_eSPI();
