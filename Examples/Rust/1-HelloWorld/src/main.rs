d1a0b1c8c7839d20779cceca13ee8862861d5d2c
Nitek <nitek@blickt.es>
2024-02-20 18:55:26 +0100

    Get rid of unsafes in Rust example

--- Examples/Rust/1-HelloWorld/src/main.rs
+++ Examples/Rust/1-HelloWorld/src/main.rs
@@ -1,17 +1,17 @@
 use display_interface_spi::SPIInterfaceNoCS;
 use embedded_graphics::{
     mono_font::{ascii::FONT_9X18, MonoTextStyle},
-    pixelcolor::Rgb666,
+    pixelcolor::Rgb565,
     prelude::*,
     text::renderer::CharacterStyle,
     text::Text,
 };
 
+use esp_idf_svc::hal::{gpio, prelude::Peripherals};
+
 use esp_idf_hal::{
     delay::Ets,
-    gpio,
-    gpio::PinDriver,
-    spi::{config::Config, config::DriverConfig, SpiDeviceDriver, SpiDriver, SPI2},
+    spi::{config::{Config, DriverConfig}, Dma, SpiDeviceDriver}, units::MegaHertz,
 };
 
 use mipidsi::Builder;
@@ -25,53 +25,62 @@ fn main() -> Result<(), Box<dyn Error>> {
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
+    let peripherals = Peripherals::take().unwrap();
+    let pins = peripherals.pins;
+
     // Reset, -1 or 4
-    let rst = PinDriver::input_output_od(unsafe { gpio::Gpio4::new() })?;
+    let rst = gpio::PinDriver::output(pins.gpio4)?;
     // Data Command control pin
-    let dc = PinDriver::input_output_od(unsafe { gpio::Gpio2::new() })?;
+    let dc = gpio::PinDriver::output(pins.gpio2)?;
 
     // Espressif built-in delay provider for small delays
     let mut delay = Ets;
 
     // Pin 14, Serial Clock
-    let sclk = unsafe { gpio::Gpio14::new() };
-    let spi = unsafe { SPI2::new() };
+    let sclk = pins.gpio14;
+    let spi = peripherals.spi2;
     // Pin 13, MOSI, Master Out Slave In
-    let sdo = unsafe { gpio::Gpio13::new() };
+    let sdo = pins.gpio13;
     // Pin 12, MISO, Master In Slave Out
-    let sdi = unsafe { gpio::Gpio12::new() };
+    let sdi = pins.gpio12;
 
-    // Serial Peripheral Interface
-    let spi = SpiDriver::new(spi, sclk, sdo, Some(sdi), &DriverConfig::new())?;
+    let cs = pins.gpio15;
 
-    // Pin 15, Chip select
-    let cs = unsafe { gpio::Gpio15::new() };
-    
-    let spi = SpiDeviceDriver::new(spi, Some(cs), &Config::new())?;
-    let di = SPIInterfaceNoCS::new(spi, dc);
-    
-    let mut display = Builder::ili9341_rgb666(di)
-        .with_color_order(mipidsi::ColorOrder::Bgr)  // Colors are shifted, Blue -> Red
-        .with_orientation(mipidsi::options::Orientation::Landscape(false))  // Mirror on text
+    let di = SPIInterfaceNoCS::new(
+        SpiDeviceDriver::new_single(
+            spi,
+            sclk,
+            sdo,
+            Some(sdi),
+            Some(cs),
+            &DriverConfig::new().dma(Dma::Disabled),
+            &Config::new().baudrate(MegaHertz(40).into()),
+        )?,
+        dc,
+    );
+
+    let mut display = Builder::ili9341_rgb565(di)
+        .with_color_order(mipidsi::ColorOrder::Rgb)
+        .with_orientation(mipidsi::options::Orientation::LandscapeInverted(true))  // Mirror on text
         .init(&mut delay, Some(rst))
         .map_err(|_| Box::<dyn Error>::from("display init"))?;
 
     // Pin 21, Backlight
-    let mut bl = PinDriver::output(unsafe { gpio::Gpio21::new() })?;
+    let mut bl = gpio::PinDriver::output(pins.gpio21)?;
     // Turn on backlight
     bl.set_high()?;
-    // Force the GPIO to hold it's high state and not go to sleep
-    // TO check: why do we need this?
-    unsafe { gpio_hold_en(21) };
+
+    // Force the GPIO to hold it's high state
+    core::mem::forget(bl);
 
     // fill the screen with black
     // TO check: this is quite slow somehow?
     display
-         .clear(Rgb666::BLACK)
+         .clear(Rgb565::BLACK)
          .map_err(|_| Box::<dyn Error>::from("clear display"))?;
 
     // Create text style
-    let mut style = MonoTextStyle::new(&FONT_9X18, Rgb666::WHITE);
+    let mut style = MonoTextStyle::new(&FONT_9X18, Rgb565::WHITE);
 
     // Position x:5, y: 10
     Text::new("Hello", Point::new(5, 10), style)
@@ -79,7 +88,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map_err(|_| Box::<dyn Error>::from("draw hello"))?;
 
     // Turn text to blue
-    style.set_text_color(Some(Rgb666::BLUE));
+    style.set_text_color(Some(Rgb565::BLUE));
     Text::new("World", Point::new(160, 26), style)
         .draw(&mut display)
         .map_err(|_| Box::<dyn Error>::from("draw world"))?;
