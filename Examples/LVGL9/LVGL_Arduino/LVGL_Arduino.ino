bbacaa193551350396cedf0923ae83b12f5613b7
Nick Reynolds <ncmreynolds@googlemail.com>
2024-02-27 18:00:30 +0000

    Initial work, touchscreen events not registered
    LVGL docs seem incomplete following the update to v9 so I can see how to make the touchscreen work. It IS polled but causes no events to happen. This behaviour has changed since LVGL 8.

---
+++ Examples/LVGL9/LVGL_Arduino/LVGL_Arduino.ino
@@ -0,0 +1,152 @@
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/integration/framework/arduino.html#arduino */
+
+#include <lvgl.h>
+
+#include <TFT_eSPI.h>
+
+#include <examples/lv_examples.h>
+#include <demos/lv_demos.h>
+
+#include <XPT2046_Touchscreen.h>
+// A library for interfacing with the touch screen
+//
+// Can be installed from the library manager (Search for "XPT2046")
+//https://github.com/PaulStoffregen/XPT2046_Touchscreen
+// ----------------------------
+// Touch Screen pins
+// ----------------------------
+
+// The CYD touch uses some non default
+// SPI pins
+
+#define XPT2046_IRQ 36
+#define XPT2046_MOSI 32
+#define XPT2046_MISO 39
+#define XPT2046_CLK 25
+#define XPT2046_CS 33
+SPIClass touchscreenSpi = SPIClass(VSPI);
+XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+uint16_t touchScreenMinimumX = 200, touchScreenMaximumX = 3700, touchScreenMinimumY = 240,touchScreenMaximumY = 3800;
+
+/*Set to your screen resolution*/
+#define TFT_HOR_RES   320
+#define TFT_VER_RES   240
+
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+
+#if LV_USE_LOG != 0
+void my_print( lv_log_level_t level, const char * buf )
+{
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
+{
+    /*Copy `px map` to the `area`*/
+
+    /*For example ("my_..." functions needs to be implemented by you)
+    uint32_t w = lv_area_get_width(area);
+    uint32_t h = lv_area_get_height(area);
+
+    my_set_window(area->x1, area->y1, w, h);
+    my_draw_bitmaps(px_map, w * h);
+     */
+
+    /*Call it to tell LVGL you are ready*/
+    lv_disp_flush_ready(disp);
+}
+/*Read the touchpad*/
+//{
+    /*For example  ("my_..." functions needs to be implemented by you)
+    int32_t x, y;
+    bool touched = my_get_touch( &x, &y );
+
+    if(!touched) {
+        data->state = LV_INDEV_STATE_RELEASED;
+    } else {
+        data->state = LV_INDEV_STATE_PRESSED;
+
+        data->point.x = x;
+        data->point.y = y;
+    }
+     */
+//}
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+//void my_touchpad_read( lv_indev_drv_t * indev_drv, lv_indev_data_t * data )
+{
+  if(touchscreen.touched())
+  {
+    Serial.print("Touch ");
+    TS_Point p = touchscreen.getPoint();
+    //Some very basic auto calibration so it doesn't go out of range
+    if(p.x < touchScreenMinimumX) touchScreenMinimumX = p.x;
+    if(p.x > touchScreenMaximumX) touchScreenMaximumX = p.x;
+    if(p.y < touchScreenMinimumY) touchScreenMinimumY = p.y;
+    if(p.y > touchScreenMaximumY) touchScreenMaximumY = p.y;
+    //Map this to the pixel position
+    data->point.x = map(p.x,touchScreenMinimumX,touchScreenMaximumX,1,TFT_HOR_RES); /* Touchscreen X calibration */
+    data->point.y = map(p.y,touchScreenMinimumY,touchScreenMaximumY,1,TFT_VER_RES); /* Touchscreen Y calibration */
+    data->state = LV_INDEV_STATE_PRESSED;
+
+    Serial.print("x ");
+    Serial.print(data->point.x);
+    Serial.print(" y ");
+    Serial.print(data->point.y);
+    Serial.println();
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
+
+lv_indev_t * indev; //Touchscreen input device
+uint8_t* draw_buf;  //draw_buf is allocated on heap otherwise the static area is too big on ESP32 at compile
+
+void setup()
+{
+  //Some basic info on the Serial console
+  String LVGL_Arduino = "Hello Arduino! ";
+  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+  Serial.begin( 115200 );
+  Serial.println( LVGL_Arduino );
+    
+  //Initialise the touchscreen
+  touchscreenSpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS); /* Start second SPI bus for touchscreen */
+  touchscreen.begin(touchscreenSpi); /* Touchscreen init */
+  touchscreen.setRotation(1); /* Landscape orientation */
+
+  //Initialise LVGL
+  lv_init();
+  draw_buf = new uint8_t[DRAW_BUF_SIZE];
+  lv_display_t * disp;
+  disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, DRAW_BUF_SIZE);
+
+  //Initialize the XPT2046 input device driver
+  indev = lv_indev_create();
+  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);  
+  lv_indev_set_read_cb(indev, my_touchpad_read);
+  lv_indev_enable(indev, true);
+  lv_indev_set_mode(indev, LV_INDEV_MODE_TIMER);
+
+
+  //Add the large standard widgets demo
+  lv_demo_widgets();
+
+  //Done
+  Serial.println( "Setup done" );
+}
+
+void loop()
+{
+    //lv_indev_read(indev); //Read the touchscreen
+    lv_timer_handler(); //Update the UI
+    delay(5);
+}
