ca7dcdcedde281595cfcf7b90333259b0432b123
Nitek <nitek@blickt.es>
2024-03-21 18:45:11 +0100

    Also update User_setup

--- DisplayConfig/CYD2USB/User_Setup.h
+++ DisplayConfig/CYD2USB/User_Setup.h
@@ -43,7 +43,7 @@
 
 // Only define one driver, the other ones must be commented out
 // #define ILI9341_DRIVER       // Generic driver for common displays
-#define ILI9341_2_DRIVER // Alternative ILI9341 driver, see https://github.com/Bodmer/TFT_eSPI/issues/1172
+#define ILI9342_DRIVER
 // #define ST7735_DRIVER      // Define additional parameters below for this display
 // #define ILI9163_DRIVER     // Define additional parameters below for this display
 // #define S6D02A1_DRIVER
@@ -84,11 +84,11 @@
 // #define TFT_WIDTH  80
 // #define TFT_WIDTH  128
 // #define TFT_WIDTH  172 // ST7789 172 x 320
-#define TFT_WIDTH 240 // ST7789 240 x 240 and 240 x 320
+// #define TFT_WIDTH 240 // ST7789 240 x 240 and 240 x 320
 // #define TFT_HEIGHT 160
 // #define TFT_HEIGHT 128
 // #define TFT_HEIGHT 240 // ST7789 240 x 240
-#define TFT_HEIGHT 320 // ST7789 240 x 320
+// #define TFT_HEIGHT 320 // ST7789 240 x 320
 // #define TFT_HEIGHT 240 // GC9A01 240 x 240
 
 // For ST7735 ONLY, define the type of display, originally this was based on the
@@ -112,7 +112,7 @@
 // If colours are inverted (white shows as black) then uncomment one of the next
 // 2 lines try both options, one of the options should correct the inversion.
 
-#define TFT_INVERSION_ON
+// #define TFT_INVERSION_ON
 // #define TFT_INVERSION_OFF
 
 // ##################################################################################
